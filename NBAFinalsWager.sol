feb7d3a972d9be077dfff2c20cd9c2d51a221b1c
Oscar Zhu <oscar@yvrmail.net>
2022-06-04 03:26:22 -0700

    added keccak256 to compare strings

--- NBAFinalsWager.sol
+++ NBAFinalsWager.sol
@@ -12,6 +12,12 @@ contract NBAFinalsWager is Owner {
     }
     Status public status;
 
+    struct Profile {
+        string team;
+        uint amount;
+    }
+    mapping (address => Profile) public profile;
+
     struct TeamA {
         string name;
         address[] votes;
@@ -30,29 +36,43 @@ contract NBAFinalsWager is Owner {
         setTeamName(_nameA, _nameB);
     }
 
-    function viewTeam(uint8 team) 
+    function viewTeam(string memory team) 
         external 
         view 
         returns (string memory, uint, uint) 
     {
-        require(team == 0 || team == 1, "Invalid team value");
-        if (team == 0) {
+        require(
+            keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamA.name)) 
+            || keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamB.name)), 
+            "Invalid team value");
+        if (keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamA.name))) 
             return (teamA.name, teamA.votes.length, teamA.amount);
-        } else {
+        else {
             return (teamB.name, teamB.votes.length, teamB.amount);
         }
     }
 
-    function wage(uint8 team) public payable {
-        require(team == 0 || team == 1, "Invalid team value");
-        require(msg.value > 0, "You must include funds to wage");
-
-        deposit(team, msg.value, msg.sender);
-        
+    function viewCurrentBet() external view returns (string memory, uint) {
+        return (profile[msg.sender].team, profile[msg.sender].amount);
     }
 
-    function deposit(uint8 team, uint256 value, address sender) internal {
-        if (team == 0) {
+    function wage(string memory team) public payable {
+        Profile storage sender = profile[msg.sender];
+
+        require(
+            keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamA.name)) 
+            || keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamB.name)), 
+            "Invalid team value");
+        require(msg.value > 0, "You must include funds to wage");
+
+        sender.team = team;
+        sender.amount = msg.value;
+
+        deposit(team, msg.value, msg.sender); 
+    }
+
+    function deposit(string memory team, uint256 value, address sender) internal {
+        if (keccak256(abi.encodePacked(team)) == keccak256(abi.encodePacked(teamA.name))) {
             teamA.votes.push(address(sender));
             teamA.amount += value;
         } else {
